6bb374986050cdb0128684a9dcd533525120e509
lee <lipk@sztechcloud.com.cn>
2023-04-13 15:50:35 +0800

    feat: stripe background

--- src/views/Background.vue
+++ src/views/Background.vue
@@ -6,22 +6,22 @@
 
 <style scoped>
 .bg-box {
+  position: relative;
   width: 200px;
   height: 100px;
   margin: 50px auto 0;
   border: 1px solid red;
-  background-image: linear-gradient(30deg, rgba(108, 128, 151, 0.1) 0%, rgba(108, 128, 151, 0.1) 33%, transparent 33%);
-  background-size: 15px 1px;
+  overflow: hidden;
 }
-.stripe-bg::before {
-  transform: rotate(45deg);
-  content: '';
-  top: -100px;
-  left: -100px;
-  bottom: -100px;
-  right: -100px;
+.bg-box::before {
   position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: -100px;
+  content: '';
   background-image: linear-gradient(30deg, rgba(108, 128, 151, 0.1) 0%, rgba(108, 128, 151, 0.1) 33%, transparent 33%);
   background-size: 15px 1px;
+  transform: skewx(45deg) translateX(-50px);
 }
 </style>
